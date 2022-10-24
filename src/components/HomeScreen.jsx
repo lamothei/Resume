b292510988c9c1af234e92ebf110818262cb01ea
Ian <jlmamothei@gmail.com>
2022-10-24 07:48:06 -0400

    Start of a resume website

---
+++ src/components/HomeScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HomeScreen() {
+  return (
+        <div className='bg-bridge h-full w-full bg-no-repeat bg-cover bg-center'>
+            <div className=''>
+                
+            </div>
+        </div>
+  )
+}
+
+export default HomeScreen
