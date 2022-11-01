71d211a8052af2b781864e3cff79bb8eba666767
Ian <jlmamothei@gmail.com>
2022-11-01 09:55:42 -0400

    added services file

---
+++ src/components/Services.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+function Services() {
+  return (
+    <div className="h-full w-full bg-no-repeat bg-cover bg-center">
+      <div className="flex items-center justify-center w-full h-full">
+        <div className="grid grid-rows-2 gap-2">
+          <div className="row-span-2">
+            <div className="text-red-500">Services</div>
+            <div className="font-bold text-4xl">Quality Services</div>
+          </div>
+          <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div class="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
+              <div class="mb-8">Image</div>
+              <div class="text-center">
+                <p class="text-xl text-gray-700 font-bold mb-2">text1</p>
+                <p class="text-base text-gray-400 font-normal">text2</p>
+              </div>
+            </div>
+            <div class="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
+              <div class="mb-8">Image</div>
+              <div class="text-center">
+                <p class="text-xl text-gray-700 font-bold mb-2">text1</p>
+                <p class="text-base text-gray-400 font-normal">text2</p>
+              </div>
+            </div>
+            <div class="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
+              <div class="mb-8">Image</div>
+              <div class="text-center">
+                <p class="text-xl text-gray-700 font-bold mb-2">text1</p>
+                <p class="text-base text-gray-400 font-normal">text2</p>
+              </div>
+            </div>
+            <div class="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
+              <div class="mb-8">Image</div>
+              <div class="text-center">
+                <p class="text-xl text-gray-700 font-bold mb-2">text1</p>
+                <p class="text-base text-gray-400 font-normal">text2</p>
+              </div>
+            </div>
+            <div class="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
+              <div class="mb-8">Image</div>
+              <div class="text-center">
+                <p class="text-xl text-gray-700 font-bold mb-2">text1</p>
+                <p class="text-base text-gray-400 font-normal">text2</p>
+              </div>
+            </div>
+            <div class="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
+              <div class="mb-8">Image</div>
+              <div class="text-center">
+                <p class="text-xl text-gray-700 font-bold mb-2">text1</p>
+                <p class="text-base text-gray-400 font-normal">text2</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Services;
